ec52b1c62435e6e4d587bcaa1f58ed3d74525e80
ZezinZika <zelumendonca@gmail.com>
2025-10-16 11:40:31 +0200

    Initial commit: ported from test repo

---
+++ build/stm32h7xx_ll_usb.d
@@ -0,0 +1,70 @@
+build/stm32h7xx_ll_usb.o: \
+ ../../Drivers/stm32h7xx_hal_driver/Src/stm32h7xx_ll_usb.c \
+ ../../Drivers/stm32h7xx_hal_driver/Inc/stm32h7xx_hal.h \
+ Inc/stm32h7xx_hal_conf.h \
+ ../../Drivers/stm32h7xx_hal_driver/Inc/stm32h7xx_hal_rcc.h \
+ ../../Drivers/stm32h7xx_hal_driver/Inc/stm32h7xx_hal_def.h \
+ ../../Drivers/cmsis_device_h7/Include/stm32h7xx.h \
+ ../../Drivers/cmsis_device_h7/Include/stm32h743xx.h \
+ ../../Drivers/CMSIS/Include/core_cm7.h \
+ ../../Drivers/CMSIS/Include/cmsis_version.h \
+ ../../Drivers/CMSIS/Include/cmsis_compiler.h \
+ ../../Drivers/CMSIS/Include/cmsis_gcc.h \
+ ../../Drivers/CMSIS/Include/mpu_armv7.h \
+ ../../Drivers/cmsis_device_h7/Include/system_stm32h7xx.h \
+ ../../Drivers/stm32h7xx_hal_driver/Inc/Legacy/stm32_hal_legacy.h \
+ ../../Drivers/stm32h7xx_hal_driver/Inc/stm32h7xx_hal_rcc_ex.h \
+ ../../Drivers/stm32h7xx_hal_driver/Inc/stm32h7xx_hal_gpio.h \
+ ../../Drivers/stm32h7xx_hal_driver/Inc/stm32h7xx_hal_gpio_ex.h \
+ ../../Drivers/stm32h7xx_hal_driver/Inc/stm32h7xx_hal_dma.h \
+ ../../Drivers/stm32h7xx_hal_driver/Inc/stm32h7xx_hal_dma_ex.h \
+ ../../Drivers/stm32h7xx_hal_driver/Inc/stm32h7xx_hal_mdma.h \
+ ../../Drivers/stm32h7xx_hal_driver/Inc/stm32h7xx_hal_exti.h \
+ ../../Drivers/stm32h7xx_hal_driver/Inc/stm32h7xx_hal_cortex.h \
+ ../../Drivers/stm32h7xx_hal_driver/Inc/stm32h7xx_hal_fdcan.h \
+ ../../Drivers/stm32h7xx_hal_driver/Inc/stm32h7xx_hal_flash.h \
+ ../../Drivers/stm32h7xx_hal_driver/Inc/stm32h7xx_hal_flash_ex.h \
+ ../../Drivers/stm32h7xx_hal_driver/Inc/stm32h7xx_hal_hsem.h \
+ ../../Drivers/stm32h7xx_hal_driver/Inc/stm32h7xx_hal_i2c.h \
+ ../../Drivers/stm32h7xx_hal_driver/Inc/stm32h7xx_hal_i2c_ex.h \
+ ../../Drivers/stm32h7xx_hal_driver/Inc/stm32h7xx_hal_pwr.h \
+ ../../Drivers/stm32h7xx_hal_driver/Inc/stm32h7xx_hal_pwr_ex.h \
+ ../../Drivers/stm32h7xx_hal_driver/Inc/stm32h7xx_hal_tim.h \
+ ../../Drivers/stm32h7xx_hal_driver/Inc/stm32h7xx_hal_tim_ex.h \
+ ../../Drivers/stm32h7xx_hal_driver/Inc/stm32h7xx_hal_pcd.h \
+ ../../Drivers/stm32h7xx_hal_driver/Inc/stm32h7xx_ll_usb.h \
+ ../../Drivers/stm32h7xx_hal_driver/Inc/stm32h7xx_hal_pcd_ex.h
+../../Drivers/stm32h7xx_hal_driver/Inc/stm32h7xx_hal.h:
+Inc/stm32h7xx_hal_conf.h:
+../../Drivers/stm32h7xx_hal_driver/Inc/stm32h7xx_hal_rcc.h:
+../../Drivers/stm32h7xx_hal_driver/Inc/stm32h7xx_hal_def.h:
+../../Drivers/cmsis_device_h7/Include/stm32h7xx.h:
+../../Drivers/cmsis_device_h7/Include/stm32h743xx.h:
+../../Drivers/CMSIS/Include/core_cm7.h:
+../../Drivers/CMSIS/Include/cmsis_version.h:
+../../Drivers/CMSIS/Include/cmsis_compiler.h:
+../../Drivers/CMSIS/Include/cmsis_gcc.h:
+../../Drivers/CMSIS/Include/mpu_armv7.h:
+../../Drivers/cmsis_device_h7/Include/system_stm32h7xx.h:
+../../Drivers/stm32h7xx_hal_driver/Inc/Legacy/stm32_hal_legacy.h:
+../../Drivers/stm32h7xx_hal_driver/Inc/stm32h7xx_hal_rcc_ex.h:
+../../Drivers/stm32h7xx_hal_driver/Inc/stm32h7xx_hal_gpio.h:
+../../Drivers/stm32h7xx_hal_driver/Inc/stm32h7xx_hal_gpio_ex.h:
+../../Drivers/stm32h7xx_hal_driver/Inc/stm32h7xx_hal_dma.h:
+../../Drivers/stm32h7xx_hal_driver/Inc/stm32h7xx_hal_dma_ex.h:
+../../Drivers/stm32h7xx_hal_driver/Inc/stm32h7xx_hal_mdma.h:
+../../Drivers/stm32h7xx_hal_driver/Inc/stm32h7xx_hal_exti.h:
+../../Drivers/stm32h7xx_hal_driver/Inc/stm32h7xx_hal_cortex.h:
+../../Drivers/stm32h7xx_hal_driver/Inc/stm32h7xx_hal_fdcan.h:
+../../Drivers/stm32h7xx_hal_driver/Inc/stm32h7xx_hal_flash.h:
+../../Drivers/stm32h7xx_hal_driver/Inc/stm32h7xx_hal_flash_ex.h:
+../../Drivers/stm32h7xx_hal_driver/Inc/stm32h7xx_hal_hsem.h:
+../../Drivers/stm32h7xx_hal_driver/Inc/stm32h7xx_hal_i2c.h:
+../../Drivers/stm32h7xx_hal_driver/Inc/stm32h7xx_hal_i2c_ex.h:
+../../Drivers/stm32h7xx_hal_driver/Inc/stm32h7xx_hal_pwr.h:
+../../Drivers/stm32h7xx_hal_driver/Inc/stm32h7xx_hal_pwr_ex.h:
+../../Drivers/stm32h7xx_hal_driver/Inc/stm32h7xx_hal_tim.h:
+../../Drivers/stm32h7xx_hal_driver/Inc/stm32h7xx_hal_tim_ex.h:
+../../Drivers/stm32h7xx_hal_driver/Inc/stm32h7xx_hal_pcd.h:
+../../Drivers/stm32h7xx_hal_driver/Inc/stm32h7xx_ll_usb.h:
+../../Drivers/stm32h7xx_hal_driver/Inc/stm32h7xx_hal_pcd_ex.h:
---
+++ build/timers.d
@@ -0,0 +1,25 @@
+build/timers.o: ../../Middlewares/Third_Party/FreeRTOS/timers.c \
+ ../../Middlewares/Third_Party/FreeRTOS/include/FreeRTOS.h \
+ Inc/FreeRTOSConfig.h \
+ ../../Middlewares/Third_Party/FreeRTOS/include/projdefs.h \
+ ../../Middlewares/Third_Party/FreeRTOS/include/portable.h \
+ ../../Middlewares/Third_Party/FreeRTOS/include/deprecated_definitions.h \
+ ../../Middlewares/Third_Party/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h \
+ ../../Middlewares/Third_Party/FreeRTOS/include/mpu_wrappers.h \
+ ../../Middlewares/Third_Party/FreeRTOS/include/task.h \
+ ../../Middlewares/Third_Party/FreeRTOS/include/list.h \
+ ../../Middlewares/Third_Party/FreeRTOS/include/queue.h \
+ ../../Middlewares/Third_Party/FreeRTOS/include/task.h \
+ ../../Middlewares/Third_Party/FreeRTOS/include/timers.h
+../../Middlewares/Third_Party/FreeRTOS/include/FreeRTOS.h:
+Inc/FreeRTOSConfig.h:
+../../Middlewares/Third_Party/FreeRTOS/include/projdefs.h:
+../../Middlewares/Third_Party/FreeRTOS/include/portable.h:
+../../Middlewares/Third_Party/FreeRTOS/include/deprecated_definitions.h:
+../../Middlewares/Third_Party/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h:
+../../Middlewares/Third_Party/FreeRTOS/include/mpu_wrappers.h:
+../../Middlewares/Third_Party/FreeRTOS/include/task.h:
+../../Middlewares/Third_Party/FreeRTOS/include/list.h:
+../../Middlewares/Third_Party/FreeRTOS/include/queue.h:
+../../Middlewares/Third_Party/FreeRTOS/include/task.h:
+../../Middlewares/Third_Party/FreeRTOS/include/timers.h:
