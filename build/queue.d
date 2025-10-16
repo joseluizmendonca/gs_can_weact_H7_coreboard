56babce04d70a13090c78501771e0f8a66f83a5b
ZezinZika <zelumendonca@gmail.com>
2025-10-16 12:04:34 +0200

    Ported over from test repo H743 project

---
+++ build/queue.d
@@ -0,0 +1,23 @@
+build/queue.o: ../../Middlewares/Third_Party/FreeRTOS/queue.c \
+ ../../Middlewares/Third_Party/FreeRTOS/include/FreeRTOS.h \
+ Inc/FreeRTOSConfig.h \
+ ../../Middlewares/Third_Party/FreeRTOS/include/projdefs.h \
+ ../../Middlewares/Third_Party/FreeRTOS/include/portable.h \
+ ../../Middlewares/Third_Party/FreeRTOS/include/deprecated_definitions.h \
+ ../../Middlewares/Third_Party/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h \
+ ../../Middlewares/Third_Party/FreeRTOS/include/mpu_wrappers.h \
+ ../../Middlewares/Third_Party/FreeRTOS/include/task.h \
+ ../../Middlewares/Third_Party/FreeRTOS/include/list.h \
+ ../../Middlewares/Third_Party/FreeRTOS/include/queue.h \
+ ../../Middlewares/Third_Party/FreeRTOS/include/task.h
+../../Middlewares/Third_Party/FreeRTOS/include/FreeRTOS.h:
+Inc/FreeRTOSConfig.h:
+../../Middlewares/Third_Party/FreeRTOS/include/projdefs.h:
+../../Middlewares/Third_Party/FreeRTOS/include/portable.h:
+../../Middlewares/Third_Party/FreeRTOS/include/deprecated_definitions.h:
+../../Middlewares/Third_Party/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h:
+../../Middlewares/Third_Party/FreeRTOS/include/mpu_wrappers.h:
+../../Middlewares/Third_Party/FreeRTOS/include/task.h:
+../../Middlewares/Third_Party/FreeRTOS/include/list.h:
+../../Middlewares/Third_Party/FreeRTOS/include/queue.h:
+../../Middlewares/Third_Party/FreeRTOS/include/task.h:
